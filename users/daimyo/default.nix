b1c4a54c245b2a375392e31cab9eda48b9a3c531
damien-bafile <bafile.damien@gmail.com>
2025-07-09 18:18:04 +0800

    Add backup extension for home-manager file conflicts

--- users/daimyo/default.nix
+++ users/daimyo/default.nix
@@ -11,5 +11,8 @@
   # Home Manager configuration
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+
+  # Backup existing files instead of failing
+  home-manager.backupFileExtension = "backup";
   home-manager.users.daimyo = import ./home.nix;
 }
